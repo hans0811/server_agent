e21095485e10c430ccdbe78d14353d1f3a2c36ed
hans <chaimarket0811@gmail.com>
2025-03-20 14:47:11 +0800

    update server, agent and testing

--- server/tests/test_update_config.py
+++ server/tests/test_update_config.py
@@ -1,7 +1,7 @@
 import pytest
 import requests
 
-BASE_URL = "http://localhost:5002"  # Change if your Flask app runs on a different port
+BASE_URL = "http://agentFlask:5002"  # Change if your Flask app runs on a different port
 
 
 @pytest.fixture(autouse=True)
